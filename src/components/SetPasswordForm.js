35da6747f7ec2db4622bd0ca6a0513ac23136422
cooljasonmelton <cooljasonmelton>
2020-08-21 00:12:06 -0500

    basic test version works. basic css so button causes strikethru

--- src/components/SetPasswordForm.js
+++ src/components/SetPasswordForm.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //styling
 import './SetPasswordForm.css';
 
 const SetPasswordForm = () => {
+  const [upperCase, setUpperCase] = useState(false)
+
+  const setClass = () => {
+    const classArr = ["must-line"]
+    if (upperCase) classArr.push('cross-out')
+    return classArr.join(' ')
+  }
+
   return (
     <div className="SetPasswordForm cfb">
         <h1> Update Password: </h1>
         <h3>Must contain:</h3>
-        <p>An uppercase letter</p>
+        <button onClick={()=>setUpperCase(!upperCase)}> test </button>
+
+        <div className="must-item">
+          <p className="must-text">An uppercase letter</p>
+          <div className={setClass()}></div>
+        </div>
+
         <form>
             <input type="password" onChange={null}/>
         </form>
